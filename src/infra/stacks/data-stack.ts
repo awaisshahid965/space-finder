32af8450fb5179c9dcb440e4f649327cb003c345
Muhammad Awais <awaisshahid965@gmail.com>
2024-01-27 23:54:11 +0500

    feat: added hello lambda function

--- src/infra/stacks/data-stack.ts
+++ src/infra/stacks/data-stack.ts
@@ -1,8 +1,20 @@
 import { Stack, StackProps } from 'aws-cdk-lib'
+import { ITable, Table as DynamoDBTable, AttributeType } from 'aws-cdk-lib/aws-dynamodb'
 import { Construct } from 'constructs'
+import { getSuffix } from '../utils'
 
 export class DataStack extends Stack {
+  public readonly spacesTable: ITable
+
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props)
+
+    this.spacesTable = new DynamoDBTable(this, 'SpacesTable', {
+      partitionKey: {
+        name: 'id',
+        type: AttributeType.STRING,
+      },
+      tableName: `SpacesTable-${getSuffix(this)}`,
+    })
   }
 }
