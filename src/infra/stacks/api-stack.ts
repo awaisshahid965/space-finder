12932502b0c2c6da401be74314c1be247c11d80f
Muhammad Awais <awaisshahid965@gmail.com>
2024-02-04 13:53:55 +0500

    feat: added cognito idp to api gateway

--- src/infra/stacks/api-stack.ts
+++ src/infra/stacks/api-stack.ts
@@ -1,20 +1,67 @@
 import { Stack, StackProps } from 'aws-cdk-lib'
-import { LambdaIntegration, RestApi } from 'aws-cdk-lib/aws-apigateway'
+import {
+  AuthorizationType,
+  CognitoUserPoolsAuthorizer,
+  LambdaIntegration,
+  MethodOptions,
+  Resource,
+  RestApi,
+} from 'aws-cdk-lib/aws-apigateway'
+import { IUserPool } from 'aws-cdk-lib/aws-cognito'
 import { Construct } from 'constructs'
+
+enum HttpRequestMethod {
+  GET = 'GET',
+  POST = 'POST',
+  PUT = 'PUT',
+  DELETE = 'DELETE',
+}
 
 interface ApiStackProps extends StackProps {
   spacesLanmbdaIntegration: LambdaIntegration
+  spacesUserPool: IUserPool
 }
 
 export class ApiStack extends Stack {
+  private spacesApiAuthorizerId!: string
+
   constructor(scope: Construct, id: string, props: ApiStackProps) {
     super(scope, id, props)
 
     const spacesApi = new RestApi(this, 'SpacesApi')
     const spacesApiResource = spacesApi.root.addResource('spaces')
-    spacesApiResource.addMethod('GET', props.spacesLanmbdaIntegration)
-    spacesApiResource.addMethod('POST', props.spacesLanmbdaIntegration)
-    spacesApiResource.addMethod('PUT', props.spacesLanmbdaIntegration)
-    spacesApiResource.addMethod('DELETE', props.spacesLanmbdaIntegration)
+
+    this.attachAuthorizationToSpacesApi(spacesApi, props.spacesUserPool)
+    this.linkLambdaWithResourceVerbs(spacesApiResource, props.spacesLanmbdaIntegration, [
+      HttpRequestMethod.GET,
+      HttpRequestMethod.POST,
+      HttpRequestMethod.PUT,
+      HttpRequestMethod.DELETE,
+    ])
+  }
+
+  private attachAuthorizationToSpacesApi(spacesApi: RestApi, spacesUserPool: IUserPool) {
+    const spacesApiAuthorizer = new CognitoUserPoolsAuthorizer(this, 'SpacesApiAuthorizer', {
+      cognitoUserPools: [spacesUserPool],
+      identitySource: 'method.request.header.Authorization',
+    })
+
+    spacesApiAuthorizer._attachToApi(spacesApi)
+    this.spacesApiAuthorizerId = spacesApiAuthorizer.authorizerId
+  }
+
+  private spacesApiResourceOptions(): MethodOptions {
+    return {
+      authorizationType: AuthorizationType.COGNITO,
+      authorizer: {
+        authorizerId: this.spacesApiAuthorizerId,
+      },
+    }
+  }
+
+  private linkLambdaWithResourceVerbs(resource: Resource, lambda: LambdaIntegration, httpVerbs: HttpRequestMethod[]) {
+    httpVerbs.map((httpVerb) => {
+      resource.addMethod(httpVerb, lambda, this.spacesApiResourceOptions())
+    })
   }
 }
---
+++ src/infra/stacks/auth-stack.ts
@@ -0,0 +1,45 @@
+import { CfnOutput, Stack, StackProps } from 'aws-cdk-lib'
+import { UserPool, UserPoolClient } from 'aws-cdk-lib/aws-cognito'
+import { Construct } from 'constructs'
+
+class AuthStack extends Stack {
+  public userPool!: UserPool
+  private userPoolClient!: UserPoolClient
+
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props)
+    this.createUserPool()
+    this.createUserPoolClient()
+  }
+
+  private createUserPool() {
+    this.userPool = new UserPool(this, 'SpaceUserPool', {
+      selfSignUpEnabled: true,
+      signInAliases: {
+        username: true,
+        email: true,
+      },
+    })
+
+    new CfnOutput(this, 'SpaceUserPoolId', {
+      value: this.userPool.userPoolId,
+    })
+  }
+
+  private createUserPoolClient() {
+    this.userPoolClient = this.userPool.addClient('SpaceUserPoolClient', {
+      authFlows: {
+        adminUserPassword: true,
+        custom: true,
+        userPassword: true,
+        userSrp: true,
+      },
+    })
+
+    new CfnOutput(this, 'SpaceUserPoolClientId', {
+      value: this.userPoolClient.userPoolClientId,
+    })
+  }
+}
+
+export default AuthStack
