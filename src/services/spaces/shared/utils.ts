6025a6058f7df69aff9f42ea9b4b36e0eb4a082f
Muhammad Awais <awaisshahid965@gmail.com>
2024-02-18 11:23:35 +0500

    feat: added cors permissions to resource and lambda

--- src/services/spaces/shared/utils.ts
+++ src/services/spaces/shared/utils.ts
@@ -2,5 +2,9 @@ export const sendServerResponse = <T extends object>(statusCode: number, body: T
   return {
     statusCode,
     body: JSON.stringify(body),
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': '*',
+    },
   }
 }
